d9d3b6ff527f42a6c786d7b110e6fd990d0ee3f5
Tobias Röschmann <tobias.roeschmann@helmholtz-berlin.de>
2024-05-30 11:03:47 +0200

    version that allows to retrieve links via m_def

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+import jmespath
+
+link_a = "ce_nome_s.CE_NOME_Chronoamperometry"
+link_b = "ce_nome_s.CE_NOME_Chronocoulometry"
+
+def split_m_def(m_def):
+        splitted_m_def = m_def.split(".")
+        return splitted_m_def
+
+
+def get_basesection(path_to_section):
+    base_sec = path_to_section['base_sections'][0]
+    base_sec_split = base_sec.split("/")
+    path_2 = base_sec_split[2]
+    path_3 = base_sec_split[4]
+    return base_sec_split
+    
+def t_link_retrieval(m_def):
+    splitted_m_def = split_m_def(m_def)
+    with open("artifacts_ce.json") as f:
+        data = json.load(f)
+        path_to_package = jmespath.search("[?name =='{}']".format(splitted_m_def[-2]), data["metainfo"]["packages"])[0]
+        path_to_section_defs = jmespath.search("[?name =='{}']".format(splitted_m_def[-1]), path_to_package["section_definitions"])[0]
+        base_sec_split =  get_basesection(path_to_section_defs)
+        path_2 = base_sec_split[2]
+        path_3 = base_sec_split[4]
+        path_to_link = jmespath.search("[?m_parent_index==`{}`]".format(path_2), data["metainfo"]["packages"])[0]
+        path_to_link_2 = jmespath.search("[?m_parent_index==`{}`]".format(path_3), path_to_link["section_definitions"])[0]
+
+    return path_to_link_2["links"]
+
+print(t_link_retrieval(link_a))
--- test.py
+++ test.py
@@ -1,3 +1,28 @@
-a = "test.liNk.Doener"
-print(a.lower())
-print("section: "+a+"|||| res "+a)
+#ce_nome_s.CE_NOME_CyclicVoltammetry
+import json
+import jmespath
+from link_api_calls import *
+from final_main import *
+link_a = "ce_nome_s.CE_NOME_Chronoamperometry"
+link_b = "ce_nome_s.CE_NOME_Chronocoulometry"
+m_def = split_m_def(link_b)
+print(m_def)
+with open("artifacts_ce.json") as f:
+    data = json.load(f)
+    path_to_package = jmespath.search("[?name =='{}']".format(m_def[-2]), data["metainfo"]["packages"])[0]
+    path_to_section_defs = jmespath.search("[?name =='{}']".format(m_def[-1]), path_to_package["section_definitions"])[0]
+    print(path_to_section_defs['base_sections'])
+
+base_sec = path_to_section_defs['base_sections'][0]
+base_sec_split = base_sec.split("/")
+path_2 = base_sec_split[2]
+path_3 = base_sec_split[4]
+
+path_to_link = jmespath.search("[?m_parent_index==`{}`]".format(path_2), data["metainfo"]["packages"])[0]
+path_to_link_2 = jmespath.search("[?m_parent_index==`{}`]".format(path_3), path_to_link["section_definitions"])[0]
+path_to_link_2 = jmespath.search("[?m_parent_index==`{}`]".format(path_3), path_to_link["subsections"])[0]
+
+####Für Quantitäten [sub_sections] hinzufügen!
+
+
+print(path_to_link_2["links"])
